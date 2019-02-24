e2b934491db1c9a8a2e604a32bd65e94e505cff9
harudinash <46379892+harudinash@users.noreply.github.com>
2019-02-24 09:21:21 +0530

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+   int i,j,k,l=0,x,y,z[100000];
+   char a[100];
+   printf("Input :\n");
+   scanf("%d",&x);
+  
+   printf("Output :\n");
+ while(x)
+ 
+ {
+     y=x%10;
+     k=y*y;
+     l=l+k;
+     x=x/10;
+ }
+ printf("%d",l);
+    return 0;
+}
