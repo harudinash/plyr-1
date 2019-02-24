f1305e315cb8afe962495e21a7cee4bf04fa0c08
harudinash <46379892+harudinash@users.noreply.github.com>
2019-02-24 08:50:46 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+   int i,j,k,l;
+   char a[100];
+   printf("Input :\n");
+   scanf("%s",a);
+   printf("Output :\n");
+  if((a[0]=='S')||(a[0]=='s'))
+  {
+      printf("yes");
+  }
+  else
+  printf("no");
+    return 0;
+}
