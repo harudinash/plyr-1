d82ff9b854c7fe200977f2e605b325fc05c64907
harudinash <46379892+harudinash@users.noreply.github.com>
2019-03-09 23:09:28 +0530

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k,l,m,n;
+    char a[1000],b[1000];
+    printf("Input :\n");
+    //gets(a);
+    scanf("%d",&m);
+    printf("output :\n");
+     for(i=2;i<m;i++)
+     {
+         if(m%i==0)
+         {
+             n=1;
+             
+         }
+     }
+     if(n==1)
+     printf("yes");
+     else
+     printf("no");
+    return 0;
+}
