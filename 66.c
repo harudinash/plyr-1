b46f452924451701e5cf58cea0abcd507da3c0ec
harudinash <46379892+harudinash@users.noreply.github.com>
2019-03-10 18:20:04 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int i,j,k,l,m,n,q,f[10000],p,a,b[10000];
+    char e[100],c[1000];
+    printf("Input :\n");
+   scanf("%d%d",&n,&p);
+   /*for(i=0;i<n;i++)
+   scanf("%d",&b[i]);*/
+   for(i=0;i<n;i++)
+           scanf("%d",&f[i]);
+   
+    printf("Output :\n");
+   
+   
+   for(i=0;i<n;i++)
+   {
+       m=0;
+       for(j=0;j<n;j++)
+       {
+           if(f[j]==f[i])
+           {
+               m++;
+           }
+       }
+       if(m==p)
+       {
+           printf("%d",f[i]);
+           f[i]='\0';
+       }
+   }
+    return 0;
+}
