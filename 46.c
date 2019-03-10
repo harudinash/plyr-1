005282b18c497298af2ceacb872f817bd9582e89
harudinash <46379892+harudinash@users.noreply.github.com>
2019-03-10 08:28:21 +0530

    Create 46.c

---
+++ 46.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char * argv[])
+{
+    
+    int a;
+    double value;
+    double result;
+    printf("Input :\n");
+scanf("%d",&a);
+printf("Output :\n");
+    value = a;
+    result = sin(value);
+    printf("%f", result);
+
+    return 0;
+}
